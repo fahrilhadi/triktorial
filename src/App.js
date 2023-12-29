9a37ebe0d8c37b367449f21944151d6da896e056
Fahril Hadi <fahrilhadi77@gmail.com>
2023-12-29 12:37:07 +0700

    add BannerComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import BannerComponent from './components/BannerComponent/BannerComponent';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <BannerComponent />
     </div>
   );
 }
---
+++ src/components/BannerComponent/BannerComponent.jsx
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import '../../assets/css/custom.css'
+import '../../assets/css/bootstrap.min.css'
+
+class BannerComponent extends Component {
+  render() {
+    return (
+        <Fragment>
+            <Container fluid={true} className="bannerContainer p-0">
+                <div className="bannerOverlay">
+                    <Container className="bannerContent">
+                        <Row>
+                            <Col className="text-center">
+                                <h1 className="bannerTitle">Triktorial</h1>
+                                <h4 className="bannerSubTitle pb-3">Lorem ipsum dolor sit amet <span className="text-warning">consectetur.</span></h4>
+                                <Button variant="outline-warning rounded-5" size="lg">Learn More</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </Container>
+        </Fragment>
+    )
+  }
+}
+
+export default BannerComponent
