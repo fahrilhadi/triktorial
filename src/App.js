c8d09097d47c5e5c62ffe22de30b5f36af92a5f9
Fahril Hadi <fahrilhadi77@gmail.com>
2023-12-30 13:10:22 +0700

    add SummaryComponent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavbarComponent from './components/NavbarComponent/NavbarComponent';
 import BannerComponent from './components/BannerComponent/BannerComponent';
 import ServicesComponent from './components/ServicesComponent/ServicesComponent';
 import AnalysisComponent from './components/AnalysisComponent/AnalysisComponent';
+import SummaryComponent from './components/SummaryComponent/SummaryComponent';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BannerComponent />
       <ServicesComponent />
       <AnalysisComponent />
+      <SummaryComponent />
     </div>
   );
 }
---
+++ src/components/SummaryComponent/SummaryComponent.jsx
@@ -0,0 +1,87 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { Component, Fragment } from 'react'
+import { Card, Col, Container, Row } from 'react-bootstrap'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faLaptop } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
+
+class SummaryComponent extends Component {
+  render() {
+    return (
+        <Fragment>
+            <Container fluid={true} className="summaryBanner p-0">
+                <div className="summaryBannerOverlay">
+                    <Container className="text-center">
+                        <Row>
+                            <Col lg={8} md={6} sm={12}>
+                                <Row className="countSection">
+                                    <Col>
+                                        <FontAwesomeIcon className="iconProject" icon={faGlobe} />
+                                        <h1 className="countNumber">
+                                            <CountUp start={0} end={35000}>
+                                            {({ countUpRef, start }) => (
+                                                <VisibilitySensor onChange={start} delayedCall>
+                                                    <span ref={countUpRef} />
+                                                </VisibilitySensor>
+                                            )}
+                                            </CountUp>
+                                        </h1>
+                                        <h4 className="countTitle">Student Worldwide</h4>
+                                        <div className="lineSummary"></div>
+                                    </Col>
+                                    <Col>
+                                        <FontAwesomeIcon className="iconProject" icon={faLaptop} />
+                                        <h1 className="countNumber">
+                                            <CountUp start={0} end={22}>
+                                                {({ countUpRef, start }) => (
+                                                    <VisibilitySensor onChange={start} delayedCall>
+                                                        <span ref={countUpRef} />
+                                                    </VisibilitySensor>
+                                                )}
+                                            </CountUp>
+                                        </h1>
+                                        <h4 className="countTitle">Courses Published</h4>
+                                        <div className="lineSummary"></div>
+                                    </Col>
+                                    <Col>
+                                        <FontAwesomeIcon className="iconProject" icon={faStar} />
+                                        <h1 className="countNumber">
+                                            <CountUp start={0} end={3000}>
+                                                {({ countUpRef, start }) => (
+                                                    <VisibilitySensor onChange={start} delayedCall>
+                                                        <span ref={countUpRef} />
+                                                    </VisibilitySensor>
+                                                )}
+                                            </CountUp>
+                                        </h1>
+                                        <h4 className="countTitle">Student Reviews</h4>
+                                        <div className="lineSummary"></div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col lg={4} md={6} sm={12}>
+                                <Card className="workCard">
+                                    <Card.Body>
+                                        <Card.Title className="cardTitle mb-3">What I Have Achieved</Card.Title>
+                                            <Card.Text>
+                                                <p className="cardSubTitle"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> Requirement Gathering</p>
+                                                <p className="cardSubTitle"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> System Analysis</p>
+                                                <p className="cardSubTitle"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> Coding Testing</p>
+                                                <p className="cardSubTitle"><FontAwesomeIcon className="iconBullent" icon={faCheckSquare} /> Implementation</p>
+                                            </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </Container>
+        </Fragment>
+    )
+  }
+}
+
+export default SummaryComponent
