543d5481b8b355691e71edd0eb9db0bde87e7f83
Fahril Hadi <fahrilhadi77@gmail.com>
2024-01-01 13:32:47 +0700

    add HomePage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import NavbarComponent from './components/NavbarComponent/NavbarComponent';
-import BannerComponent from './components/BannerComponent/BannerComponent';
-import ServicesComponent from './components/ServicesComponent/ServicesComponent';
-import AnalysisComponent from './components/AnalysisComponent/AnalysisComponent';
-import SummaryComponent from './components/SummaryComponent/SummaryComponent';
-import RecentProjectsComponent from './components/RecentProjectsComponent/RecentProjectsComponent';
-import CoursesComponent from './components/CoursesComponent/CoursesComponent';
-import VideoComponent from './components/VideoComponent/VideoComponent';
-import TestimonialComponent from './components/TestimonialComponent/TestimonialComponent';
-import AboutComponent from './components/AboutComponent/AboutComponent';
-import FooterComponent from './components/FooterComponent/FooterComponent';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div>
-      <NavbarComponent />
-      <BannerComponent />
-      <ServicesComponent />
-      <AnalysisComponent />
-      <SummaryComponent />
-      <RecentProjectsComponent />
-      <CoursesComponent />
-      <VideoComponent />
-      <TestimonialComponent />
-      <AboutComponent />
-      <FooterComponent />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react'
+import NavbarComponent from '../../components/NavbarComponent/NavbarComponent';
+import BannerComponent from '../../components/BannerComponent/BannerComponent';
+import ServicesComponent from '../../components/ServicesComponent/ServicesComponent';
+import AnalysisComponent from '../../components/AnalysisComponent/AnalysisComponent';
+import SummaryComponent from '../../components/SummaryComponent/SummaryComponent';
+import RecentProjectsComponent from '../../components/RecentProjectsComponent/RecentProjectsComponent';
+import CoursesComponent from '../../components/CoursesComponent/CoursesComponent';
+import VideoComponent from '../../components/VideoComponent/VideoComponent';
+import TestimonialComponent from '../../components/TestimonialComponent/TestimonialComponent';
+import AboutComponent from '../../components/AboutComponent/AboutComponent';
+import FooterComponent from '../../components/FooterComponent/FooterComponent';
+
+class HomePage extends Component {
+  render() {
+    return (
+        <Fragment>
+            <NavbarComponent />
+            <BannerComponent />
+            <ServicesComponent />
+            <AnalysisComponent />
+            <SummaryComponent />
+            <RecentProjectsComponent />
+            <CoursesComponent />
+            <VideoComponent />
+            <TestimonialComponent />
+            <AboutComponent />
+            <FooterComponent />
+        </Fragment>
+    )
+  }
+}
+
+export default HomePage
