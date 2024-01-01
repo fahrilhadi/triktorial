86499b9693121a88a05f349f03ab54e945de3826
Fahril Hadi <fahrilhadi77@gmail.com>
2024-01-01 14:18:49 +0700

    add AboutDescriptionComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TopPageComponent from './components/TopPageComponent/TopPageComponent';
 import NavbarComponent from './components/NavbarComponent/NavbarComponent';
+import AboutDescriptionComponent from './components/AboutDescriptionComponent/AboutDescriptionComponent';
 
 function App() {
   return (
     <div>
       <NavbarComponent />
       <TopPageComponent pagetitle="About Us" />
+      <AboutDescriptionComponent />
     </div>
   );
 }
