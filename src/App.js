61f5b79d8c55f9da0cbba43154112b8b7f7fc0de
Fahril Hadi <fahrilhadi77@gmail.com>
2024-01-01 15:28:40 +0700

    add PortfolioPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import NavbarComponent from './components/NavbarComponent/NavbarComponent';
-import TopPageComponent from './components/TopPageComponent/TopPageComponent';
-import AllProjectsComponent from './components/AllProjectsComponent/AllProjectsComponent';
-import FooterComponent from './components/FooterComponent/FooterComponent';
+import PortfolioPage from './pages/PortfolioPage/PortfolioPage';
 
 function App() {
   return (
     <div>
-      <NavbarComponent />
-      <TopPageComponent pagetitle="Our Portfolio" />
-      <AllProjectsComponent />
-      <FooterComponent />
+      <PortfolioPage />
     </div>
   );
 }
---
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react'
+import NavbarComponent from '../../components/NavbarComponent/NavbarComponent'
+import TopPageComponent from '../../components/TopPageComponent/TopPageComponent'
+import AllProjectsComponent from '../../components/AllProjectsComponent/AllProjectsComponent'
+import FooterComponent from '../../components/FooterComponent/FooterComponent'
+
+class PortfolioPage extends Component {
+  render() {
+    return (
+        <Fragment>
+            <NavbarComponent />
+            <TopPageComponent pagetitle="Our Portfolio" />
+            <AllProjectsComponent />
+            <FooterComponent />
+        </Fragment>
+    )
+  }
+}
+
+export default PortfolioPage
