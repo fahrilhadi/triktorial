5348849c7dd6b2282fe79ae99648565efc565b38
Fahril Hadi <fahrilhadi77@gmail.com>
2024-01-13 18:17:50 +0700

    home service page react API consuming

--- src/components/ServicesComponent/ServicesComponent.jsx
+++ src/components/ServicesComponent/ServicesComponent.jsx
@@ -3,9 +3,37 @@ import { Col, Container, Row } from 'react-bootstrap'
 import designIcon from '../../assets/images/ServicesImage/design.png'
 import ecommerceIcon from '../../assets/images/ServicesImage/ecommerce.png'
 import webIcon from '../../assets/images/ServicesImage/web.png'
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
 
 class ServicesComponent extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            myData:[]
+        }
+    }
+
+    componentDidMount(){
+        RestClient.GetRequest(AppUrl.Services).then(result=>{
+            this.setState({myData:result});
+        })
+    }
+
   render() {
+
+    const myList = this.state.myData;
+    const myView = myList.map(myList=>{
+        return <Col lg={4} md={6} sm={12}>
+                    <div className="serviceCard text-center">
+                        <img className="designIcon" src={myList.service_logo} />
+                        <h2 className="serviceName">{myList.service_name}</h2>
+                        <p className="serviceDescription">{myList.service_description}</p>
+                    </div>
+                </Col>
+    })
+
     return (
         <Fragment>
             <Container fluid={true} className="serviceContainer">
@@ -13,27 +41,7 @@ class ServicesComponent extends Component {
                     <h1 className="serviceMainTitle">My Services</h1>
                     <div className="bottom"></div>
                         <Row>
-                            <Col lg={4} md={6} sm={12}>
-                                <div className="serviceCard text-center">
-                                    <img className="designIcon" src={designIcon} />
-                                    <h2 className="serviceName">Web Design</h2>
-                                    <p className="serviceDescription">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                                </div>
-                            </Col>
-                            <Col lg={4} md={6} sm={12}>
-                                <div className="serviceCard text-center">
-                                    <img className="ecommerceIcon" src={ecommerceIcon} />
-                                    <h2 className="serviceName">E-Commerce</h2>
-                                    <p className="serviceDescription">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                                </div>
-                            </Col>
-                            <Col lg={4} md={6} sm={12}>
-                                <div className="serviceCard text-center">
-                                    <img className="webIcon" src={webIcon} />
-                                    <h2 className="serviceName">Web Development</h2>
-                                    <p className="serviceDescription">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                                </div>
-                            </Col>
+                            {myView}
                         </Row>
                 </Container>
             </Container>
